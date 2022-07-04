22e29b3e9efdc10a75ce9574d99b909c17f64bcb
Andrey Kisetov <rbcfff@yandex.ru>
2022-07-04 16:37:44 +0300

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+            {
+            Console.Clear();
+            Console.WriteLine("Введите положительное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string x = Convert.ToString(number);
+                if (x.Length > 2)
+                {
+                    Console.WriteLine($"-> {x[2]}");
+                }
+                else
+                {
+                    Console.WriteLine($"-> Третьей цифры нет");
+                }
+            }
+        }
+    }
+}
